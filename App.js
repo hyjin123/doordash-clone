654a0d23f59badd0e1b0894237573170bb65dea2
Sean <seanhoyeonjin@gmail.com>
2022-11-10 13:14:03 -0500

    headers is all set

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import tw from "twrnc";
 import { NavigationContainer } from "@react-navigation/native";
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
 import tw from "twrnc";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  // can change setting of navigation such as not showing the header
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white pt-5`}>
+      {/* Header */}
+      <View style={tw`flex-row pb-3 items-center mx-4`}>
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          style={tw`h-7 w-7 bg-gray-300 p-4 rounded-full`}
+        />
+        <View style={tw`flex-1 pl-3`}>
+          <Text style={tw`font-bold text-gray-400 text-xs`}>Deliver Now!</Text>
+          <Text style={tw`font-bold text-xl`}>
+            Current Location
+            <ChevronDownIcon size={20} color="#FF3008" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#FF3008" />
+      </View>
+      {/* Search Box */}
+      <View style={tw`flex-row items-center pb-2 mx-4`}>
+        <View style={tw`flex-row flex-1 mr-4 bg-gray-200 p-3`}>
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and Cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#FF3008" />
+      </View>
+
+      {/* Body */}
+      <ScrollView>
+        {/* Categories */}
+        <Categories />
+        {/* Featured - such as offers, features, etc */}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
