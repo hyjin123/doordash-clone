d8a6be0ab27cd184b861cca257859c3acfdaf6ac
Sean <seanhoyeonjin@gmail.com>
2022-11-11 14:09:05 -0500

    set up redux, users are now able to add and remove item from their cart and it reflects on the screen

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,32 +1,79 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Currency from "react-currency-formatter";
 import tw from "twrnc";
 import { urlFor } from "../sanity";
+import { PlusCircleIcon, MinusCircleIcon } from "react-native-heroicons/solid";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItemsWithId,
+} from "../features/basketSlice";
 
 const DishRow = ({ id, name, description, price, image }) => {
-  return (
-    <TouchableOpacity style={tw`bg-white border p-4 border-gray-100`}>
-      <View style={tw`flex-row`}>
-        <View style={tw`flex-1 pr-2`}>
-          <Text style={tw`text-lg mb-1`}>{name}</Text>
-          <Text style={tw`text-gray-400`}>{description}</Text>
-          <Text style={tw`text-gray-400`}>
-            <Currency quantity={price} currency="CAD" />
-          </Text>
-        </View>
+  const [isPressed, setIsPressed] = useState(false);
+  const items = useSelector((state) => selectBasketItemsWithId(state, id));
+  const dispatch = useDispatch();
 
-        <View>
-          <Image
-            style={{ borderWidth: 1, borderColor: "#F3F3F4" }}
-            source={{
-              uri: urlFor(image).url(),
-            }}
-            style={tw`h-20 w-20 bg-gray-300 p-4`}
-          />
+  // fired whenever items are added to the basket
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+
+  // fired whenever items are removed
+  const removeItemFromBasket = () => {
+    // don't allow user to remove something that doesn't exist
+    if (!items.length > 0) return;
+
+    dispatch(removeFromBasket({ id }));
+  };
+
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() => setIsPressed(!isPressed)}
+        style={tw`bg-white border p-4 border-gray-100 ${
+          isPressed && "border-b-0"
+        }`}
+      >
+        <View style={tw`flex-row`}>
+          <View style={tw`flex-1 pr-2`}>
+            <Text style={tw`text-lg mb-1`}>{name}</Text>
+            <Text style={tw`text-gray-400`}>{description}</Text>
+            <Text style={tw`text-gray-400`}>
+              <Currency quantity={price} currency="CAD" />
+            </Text>
+          </View>
+
+          <View>
+            <Image
+              style={{ borderWidth: 1, borderColor: "#F3F3F4" }}
+              source={{
+                uri: urlFor(image).url(),
+              }}
+              style={tw`h-20 w-20 bg-gray-300 p-4`}
+            />
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+
+      {isPressed && (
+        <View style={tw`bg-white px-4`}>
+          <View style={tw`flex-row items-center pb-3`}>
+            <TouchableOpacity onPress={removeItemFromBasket} style={tw`pr-2`}>
+              <MinusCircleIcon size={40} color="#FF3008" />
+            </TouchableOpacity>
+
+            <Text>{items.length}</Text>
+
+            <TouchableOpacity onPress={addItemToBasket} style={tw`pl-2`}>
+              <PlusCircleIcon size={40} color="#FF3008" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </>
   );
 };
 
---
+++ features/basketSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      // create a copy of the basket
+      let newBasket = [...state.items];
+
+      // if index/item is found, cut it out of the basket, hence reducing the count by one
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as its not in the basket!`
+        );
+      }
+
+      // replace old basket with new basket of items
+      state.items = newBasket;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+export const selectBasketItems = (state) => state.basket.items;
+
+// helper function that filters only the selected item by their id
+export const selectBasketItemsWithId = (state, id) =>
+  state.basket.items.filter((item) => item.id === id);
+
+export default basketSlice.reducer;
